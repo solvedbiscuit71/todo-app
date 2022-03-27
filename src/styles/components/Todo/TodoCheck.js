f211ff84738b9ffa8f95f78ad0f6fb34d2f72fff
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-27 09:28:16 +0530

    Added hover functionality (with linear-gradient border)

--- src/styles/components/Todo/TodoCheck.js
+++ src/styles/components/Todo/TodoCheck.js
@@ -1,19 +1,36 @@
 import styled from "@emotion/styled";
 
-export default styled.div({
+export default styled.div(props => ({
   width: 22,
   height: 22,
-  borderWidth: 1.5,
-  borderStyle: "solid",
   borderRadius: "50%",
   backgroundRepeat: "no-repeat",
-  backgroundPosition: "top 50% left 50%",
+  backgroundColor: props.theme.checkBorder,
+  position: "relative",
+  "&::after": {
+    content: '""',
+    width: 19,
+    height: 19,
+    position: "absolute",
+    top: 1.5,
+    left: 1.5,
+    borderRadius: "50%",
+    backgroundColor: props.theme.cardBg,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "top 50% left 50%",
+    backgroundImage: props.checked ? props.theme.checkBg : "",
+  },
+  "&:hover": {
+    cursor: "pointer",
+    backgroundImage: "linear-gradient(to bottom right,hsl(192, 100%, 67%),hsl(280, 87%, 65%))",
+  },
+
   "@media (min-width:769px)": {
     width: 24,
-    height: 24
+    height: 24,
+    "&::after": {
+      width: 21,
+      height: 21
+    }
   }
-},props => ({
-  borderColor: props.checked ? props.theme.cardBg : props.theme.checkBorder,
-  transition: `border-color ${props.theme.duration} ${props.theme.timeFunction}`,
-  backgroundImage: props.checked ? props.theme.checkBg : "",
 }))
